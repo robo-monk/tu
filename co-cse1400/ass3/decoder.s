5a84812a028e53262ea40bab579671ff1f9687c7
Filippos Taprantzis <filtap007@gmail.com>
2020-09-25 13:51:09 -0400

    add comments and shit

--- co-cse1400/ass3/decoder.s
+++ co-cse1400/ass3/decoder.s
@@ -8,9 +8,13 @@
 
 
 /*.include "abc_sorted.s"*/
-.include "helloWorld.s"
+/*.include "helloWorld.s"*/
+.include "final.s"
 
 .global main
+
+
+# HELPER SUBROUTINES
 
 # params: clean_block
 # ret: next address of block 
@@ -18,9 +22,11 @@ grab_nextadr:
 	# prologue
 	pushq	%rbp 			# push the base pointer (and align the stack)
 	movq	%rsp, %rbp		# copy stack pointer value to base pointer
+
 	#grab value
-	movq %rdi, %rax	
+	movq %rdi, %rax			# move block to return register rax
 	shr $32, %rax			# chop off value and times off the end of the block 16 bits to crop + 16 0 to crop from previous instruction
+
 	# epilogue
 	movq	%rbp, %rsp		# clear local variables from stack
 	popq	%rbp			# restore base pointer location 
@@ -32,10 +38,12 @@ grab_times:
 	# prologue
 	pushq	%rbp 			# push the base pointer (and align the stack)
 	movq	%rsp, %rbp		# copy stack pointer value to base pointer
+
 	# grab times
-	movq %rdi, %rax			# move chopped block to new register, because the proccess will be destructive
-	shl $32, %rax			# chop off value and times off the end of the block
-	shr $56, %rax			# chop off value and times off the end of the block
+	movq %rdi, %rax			
+	shl $32, %rax			# chop off address
+	shr $56, %rax			# shift right to compensate for the previous chopping
+
 	# epilogue
 	movq	%rbp, %rsp		# clear local variables from stack
 	popq	%rbp			# restore base pointer location 
@@ -47,10 +55,12 @@ grab_value:
 	# prologue
 	pushq	%rbp 			# push the base pointer (and align the stack)
 	movq	%rsp, %rbp		# copy stack pointer value to base pointer
+
 	# grab value 
 	movq %rdi, %rax			# move chopped block to ret register
-	shl $40, %rax			# chop off value and times off the end of the block
-	shr $56, %rax			# chop off value and times off the end of the block
+	shl $40, %rax			# chop off address + times
+	shr $56, %rax			# shift right to compensate for the previous chopping
+
 	# epilogue
 	movq	%rbp, %rsp		# clear local variables from stack
 	popq	%rbp			# restore base pointer location 
@@ -62,92 +72,74 @@ get_clean_block:
 	# prologue
 	pushq	%rbp 			# push the base pointer (and align the stack)
 	movq	%rsp, %rbp		# copy stack pointer value to base pointer
-	
-	movq $8, %rax
-	mulq %rsi
+
+	# find the block	
+	movq $8, %rax			# 8 for 8 bits
+	mulq %rsi			# 8bits * relative address stored in rsi
 	addq %rax, %rdi			# go to the correct block
-	movq (%rdi), %rax		# block to ret register
+	movq (%rdi), %rax		# contents of calculated address to rax
+	# clean it
 	shl $16, %rax			# chop the uknown-uneccessary bits
-	# epilogue
-	movq	%rbp, %rsp		# clear local variables from stack
-	popq	%rbp			# restore base pointer location 
-	ret
-
-# tests
-test:
-	# prologue
-	pushq	%rbp 			# push the base pointer (and align the stack)
-	movq	%rsp, %rbp		# copy stack pointer value to base pointer
-
-	movq %rdi, %rbx			# move to calle saved arg
-	# testing the subroutines
-	/*movq %rbx, %rdi*/
-	call grab_value
-	movq %rax, %rsi
-	movq $0, %rax
-	movq $charn, %rdi
-	call printf			# print the value
-
-	movq %rbx, %rdi
-	call grab_times
-	movq %rax, %rsi
-	movq $0, %rax
-	movq $digit, %rdi
-	call printf			# print the times
-
-	movq %rbx, %rdi
-	call grab_nextadr
-	movq %rax, %rsi
-	movq $0, %rax
-	movq $digit, %rdi
-	call printf			# print the address
 
 	# epilogue
 	movq	%rbp, %rsp		# clear local variables from stack
 	popq	%rbp			# restore base pointer location 
 	ret
+
+
+
+# MAIN CODE
+
 
 #   params: the address of the message to read
 #   ret: no
 decode:
 	# prologue
 	pushq	%rbp 			# push the base pointer (and align the stack)
+	pushq	%rbx			# push contents of rbx
+	pushq	%r12			# push contents of r12
+	pushq	%r13			# push contents of r13
+	pushq	%r14			# push contents of r14
 	movq	%rsp, %rbp		# copy stack pointer value to base pointer
 
-	movq %rdi, %rbx
-	movq $0, %r12	
+	movq %rdi, %rbx			# copy the input message to rbx
+	movq $0, %r12			# r12 stores the address of the block to print - so 0 at first
 
 	decode_block:
-		movq %r12, %rsi
-		movq %rbx, %rdi
+		movq %r12, %rsi			# param2 of get_clean_block is the relative address
+		movq %rbx, %rdi			# param1 is the message
 		call get_clean_block
-		movq %rax, %r13 		# move clean block to rcx
+		movq %rax, %r13 		# move clean block to r13
 		
-		movq %r13, %rdi
+		movq %r13, %rdi			# param1 of grab_times is a cleaned_block
 		call grab_times
-		movq %rax, %r14
+		movq %rax, %r14			# store times to print in %r14
 
 		print_val:
-			decq %r14
-			movq %r13, %rdi
+			decq %r14			# dec 1 from how many time more to print
+			movq %r13, %rdi			# param1 of grab_value is the cleaned block
 			call grab_value
-			movq %rax, %rsi
-			movq $0, %rax
-			movq $char, %rdi
+			movq %rax, %rsi			# param1 of printf is the value
+			movq $0, %rax			# no vectors
+			movq $char, %rdi		# param2 is an ASCII character formatting string
 			call printf			# print the value
 
-			cmpq $0, %r14
+			cmpq $0, %r14			# print val it again?
 			jne print_val 
 
-		movq %r13, %rdi
+		movq %r13, %rdi			# param1 of grab_nextadr is clean block	
 		call grab_nextadr
-		movq %rax, %r12
+		movq %rax, %r12			# store next address to r12
 
-		cmpq $0, %rax
+		cmpq $0, %r12			# if next address is not 0 do this again
 		jne decode_block 
 
 	# epilogue
 	movq	%rbp, %rsp		# clear local variables from stack
+	popq	%r14			# restore og r14 
+	popq	%r13			# restore og r13 
+	popq	%r12			# restore og r12 value 
+	popq	%rbx			# restore og rbx 
 	popq	%rbp			# restore base pointer location 
 	ret
 
